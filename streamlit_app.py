fb13121e39af367b73f0cbe1485288097a41bb5a
antonelli182 <32582969+antonelli182@users.noreply.github.com>
2023-08-05 14:02:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,37 @@
 import streamlit as st
 import tempfile
+import essentia
+from essentia.standard import MonoLoader, RhythmExtractor2013
 
 st.title("TITLE LYRIC APP 🎵")
-
 st.header("Upload Your Music")
 
 uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "flac", "aac", "ogg"])
 
+def analyze_mood(tempo, key, harmony, rhythm, timbre):
+    # ... (same function code as above) ...
+
 if uploaded_file is not None:
     tfile = tempfile.NamedTemporaryFile(delete=False) 
     tfile.write(uploaded_file.read())
-
     st.audio(tfile.name, format='audio')
+
+    loader = MonoLoader(filename=tfile.name)
+    audio = loader()
+
+    rhythm_extractor = RhythmExtractor2013()
+    tempo, beats = rhythm_extractor(audio)
+
+    st.write(f"Detected tempo: {tempo} beats per minute")
+
+    # Placeholder values for other parameters
+    key = 'major'
+    harmony = 'consonant'
+    rhythm = 'simple'
+    timbre = 'bright'
+
+    mood = analyze_mood(tempo, key, harmony, rhythm, timbre)
+    st.write(f"Detected mood: {mood}")
 
     if st.button('Upload'):
         st.write("Uploading...")
@@ -20,10 +40,8 @@ if uploaded_file is not None:
         # and process the audio file accordingly.
 
         progress_bar = st.progress(0)
-
         for i in range(100):
             # Update the progress bar with each iteration.
             progress_bar.progress(i + 1)
 
         st.success('Upload successful!')
-        # You can also use st.error('Upload failed. Please try again.') for error messages.
