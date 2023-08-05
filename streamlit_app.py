5137770de6ceeac01ae05f892b266c00e976bd28
antonelli182 <32582969+antonelli182@users.noreply.github.com>
2023-08-05 13:30:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import tempfile
 
-"""
-# Welcome to Streamlit!
+st.title("TITLE LYRIC APP 🎵")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.header("Upload Your Music")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "flac", "aac", "ogg"])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if uploaded_file is not None:
+    tfile = tempfile.NamedTemporaryFile(delete=False) 
+    tfile.write(uploaded_file.read())
 
+    st.audio(tfile.name, format='audio')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    if st.button('Upload'):
+        st.write("Uploading...")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        # You can include the code here to handle the upload to your server
+        # and process the audio file accordingly.
 
-    points_per_turn = total_points / num_turns
+        progress_bar = st.progress(0)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        for i in range(100):
+            # Update the progress bar with each iteration.
+            progress_bar.progress(i + 1)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        st.success('Upload successful!')
+        # You can also use st.error('Upload failed. Please try again.') for error messages.
